7ecd3fe972a57b3f11f7dfb910c5b43e891a11f4
root <tylerblake.tc@gmail.com>
2017-10-24 12:06:00 -0500

    Solution

---
+++ CPP_Solutions/UVA_12372_PackingforHolliday.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int l, w, h, cases;
+	cin >> cases;
+	for (int i = 1; i <= cases; i++)
+	{
+		cin >> l >> w >> h;
+		if (l > 20 || w > 20 || h > 20) { cout << "Case " << i << ": " << "bad" << endl; }
+		else { cout << "Case " << i << ": " << "good" << endl; }
+	}
+
+	return 0;
+}
